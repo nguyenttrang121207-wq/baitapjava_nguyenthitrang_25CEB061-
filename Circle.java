a977820ee6a5c304489421d5b357e8d6cdf7ec64
nguyenttrang121207-wq <nguyenttrang121207@gmail.com>
2025-10-10 11:03:27 +0700

    your message

---
+++ Circle.java
@@ -0,0 +1,22 @@
+public class Circle {
+    private double radius=1.0f;
+    public Circle(double r){
+        this.radius = 1.0;
+    }
+    public double getRadius() {
+        return this.radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+       return this.radius * this.radius*Math.PI;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+@Override
+    public String toString() {
+        return "Circle[radius=" + this.radius + "]";
+}
+}
