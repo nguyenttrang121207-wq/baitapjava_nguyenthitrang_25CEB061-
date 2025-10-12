2a7dc2b44ab07d0a227f4dd7570030afd49760b5
nguyenttrang121207-wq <nguyenttrang121207@gmail.com>
2025-10-12 20:15:27 +0700

    Delete MAIN.java

--- MAIN.java
+++
@@ -1,9 +0,0 @@
-public class MAIN {
-    public static void main(String[] args) {
-        heheheheh md= new heheheheh(3,7,2011);
-        md.printday();
-        md.printMonth();
-        md.printYear();
-        md.printDate();
-    }
-}
