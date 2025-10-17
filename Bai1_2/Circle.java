5604510b68c5d3102f90b798d797b453d6a9f4b0
nguyenttrang121207-wq <nguyenttrang121207@gmail.com>
2025-10-17 11:12:10 +0700

    your message

--- Bai1_2/Circle.java
+++ Bai1_2/Circle.java
@@ -1,23 +1,23 @@
-//package Bai1_2;
-// public class Circle {
-//        private double radius=1.0f;
-//        public Circle(double r){
-//            this.radius = 1.0;
-//        }
-//        public double getRadius() {
-//            return this.radius;
-//        }
-//        public void setRadius(double radius) {
-//            this.radius = radius;
-//        }
-//        public double getArea() {
-//            return this.radius * this.radius*Math.PI;
-//        }
-//        public double getCircumference() {
-//            return 2 * Math.PI * radius;
-//        }
-//        @Override
-//        public String toString() {
-//            return "Circle[radius=" + this.radius + "]";
-//        }
-//    }
+package Bai1_2;
+ public class Circle {
+        private double radius=1.0f;
+        public Circle(double r){
+            this.radius = 1.0;
+        }
+        public double getRadius() {
+            return this.radius;
+        }
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+        public double getArea() {
+            return this.radius * this.radius*Math.PI;
+        }
+        public double getCircumference() {
+            return 2 * Math.PI * radius;
+        }
+        @Override
+        public String toString() {
+            return "Circle[radius=" + this.radius + "]";
+        }
+    }
--- Bai1_2/TESTM.java
+++ Bai1_2/TESTM.java
@@ -1,20 +1,20 @@
-//package Bai1_2;
-//public class TESTM {
-//        public static void main(String[] args) {
-//            // Test Constructors and toString()
-//            Circle c1 = new Circle(1.1);
-//            System.out.println(c1);   // toString()
-//            Circle c2 = new Circle(1.1); // default constructor
-//            System.out.println(c2);
-//
-//            // Test setter and getter
-//            c1.setRadius(2.2);
-//            System.out.println(c1);      // toString()
-//            System.out.println("radius is: " + c1.getRadius());
-//
-//            // Test getArea() and getCircumference()
-//            System.out.printf("area is: %.2f%n", c1.getArea());
-//            System.out.printf("circumference is: %.2f%n", c1.getCircumference());
-//        }
-//    }
-//
+package Bai1_2;
+public class TESTM {
+        public static void main(String[] args) {
+            // Test Constructors and toString()
+            Circle c1 = new Circle(1.1);
+            System.out.println(c1);   // toString()
+            Circle c2 = new Circle(1.1); // default constructor
+            System.out.println(c2);
+
+            // Test setter and getter
+            c1.setRadius(2.2);
+            System.out.println(c1);      // toString()
+            System.out.println("radius is: " + c1.getRadius());
+
+            // Test getArea() and getCircumference()
+            System.out.printf("area is: %.2f%n", c1.getArea());
+            System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+        }
+    }
+
